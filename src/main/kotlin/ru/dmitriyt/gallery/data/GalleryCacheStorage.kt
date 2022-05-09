52c26cacb5072d93551b2bd37a6e3d32071129e5
Dmitriy Tomilov <tomilov.d.a@mail.ru>
2022-05-09 21:47:49 +0400

    disable file cache

--- src/main/kotlin/ru/dmitriyt/gallery/data/GalleryCacheStorage.kt
+++ src/main/kotlin/ru/dmitriyt/gallery/data/GalleryCacheStorage.kt
@@ -50,29 +50,31 @@ object GalleryCacheStorage {
     }
 
     suspend fun addToFileCache(key: String, image: BufferedImage) = withContext(Dispatchers.IO) {
-        val base64key = Base64.getEncoder().encodeToString(key.toByteArray(Charsets.UTF_8)).takeLast(15) + ".jpg"
-        File(CACHE_DIR).apply {
-            if (!exists()) {
-                mkdir()
-            }
-        }
-        val imageFile = File(CACHE_DIR, base64key).apply {
-            if (!exists()) {
-                createNewFile()
-            }
-        }
-        ImageIO.write(image, "jpeg", imageFile)
+        return@withContext Unit
+//        val base64key = Base64.getEncoder().encodeToString(key.toByteArray(Charsets.UTF_8)).takeLast(15) + ".jpg"
+//        File(CACHE_DIR).apply {
+//            if (!exists()) {
+//                mkdir()
+//            }
+//        }
+//        val imageFile = File(CACHE_DIR, base64key).apply {
+//            if (!exists()) {
+//                createNewFile()
+//            }
+//        }
+//        ImageIO.write(image, "jpeg", imageFile)
 //        println("saved to ${File(CACHE_DIR, base64key)}")
     }
 
     suspend fun getFromFileCache(key: String): BufferedImage? = withContext(Dispatchers.IO) {
-        val base64key = Base64.getEncoder().encodeToString(key.toByteArray(Charsets.UTF_8)).takeLast(15) + ".jpg"
-        val file = File(CACHE_DIR, base64key)
-        if (file.exists()) {
-//            println("$file")
-            ImageIO.read(file)
-        } else {
-            null
-        }
+        return@withContext null
+//        val base64key = Base64.getEncoder().encodeToString(key.toByteArray(Charsets.UTF_8)).takeLast(15) + ".jpg"
+//        val file = File(CACHE_DIR, base64key)
+//        if (file.exists()) {
+////            println("$file")
+//            ImageIO.read(file)
+//        } else {
+//            null
+//        }
     }
 }
